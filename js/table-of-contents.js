02df9f9f6140d6946ce11bd72d765f340304a770
Nathan Hibner <nhibner@gmail.com>
2014-02-11 09:49:17 -0600

    Created basic script to generate a table of contents with links to headers on the page

---
+++ js/table-of-contents.js
@@ -0,0 +1,22 @@
+$(function() {
+
+  // Only display table of contents if there is more than one header
+  if($('#toc_1').length != 1) {
+    return;
+  }
+
+  // Add table of contents title
+  $('#toc').append('<u>Page Contents:</u></br>');
+
+  // Add links to headers
+  var i = 0;
+  while($('#toc_' + i).length == 1) {
+    var id = '#toc_' + i;
+    var el = $(id);
+    var link = $('<a></a>');
+    link.attr('href', id);
+    link.text(el.text());
+    $('#toc').append(link).append('</br>');
+    i++;
+  }
+});
